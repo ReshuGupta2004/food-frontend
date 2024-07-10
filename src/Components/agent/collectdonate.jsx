5939644434d2e9e606d22e256d7898e0f049ce50
ReshuGupta2004 <riyanshigupta2004@gmail.com>
2024-07-10 22:56:04 +0530

    resolve error in collectiondata

--- src/Components/agent/collectdonate.jsx
+++ src/Components/agent/collectdonate.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Navigation from "../Sidenav/sidenav";
-import createToast from '../../../utils/toast';
+import createToast from "../../utils/toast";
 
 const CollectDonation = () => {
   const { collectionId } = useParams();
